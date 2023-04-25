e25b0fbfbc7d3e0a456ad6a5a99cb2ed378ffb02
Mahmudul Hasan <hasanmahmudul020@gmail.com>
2023-04-25 18:23:10 +0600

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContentAnimation,
   headContainerAnimation,
@@ -15,13 +16,35 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.div className="home" {...slideAnimation("left")}>
-          <motion.header>
+          <motion.header {...slideAnimation("down")}>
             <img
               src="./threejs.png"
               alt="logo"
               className="w-8 h-8 object-contain"
             ></img>
           </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden " /> DO IT
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className=" max-w-md font-normal text-gray-600 text-base ">
+                create your won brand tshirt with 3D customizing tool{" "}
+                <strong>unlish your imagination</strong> define your style
+              </p>
+              <CustomButton 
+              type="filled"
+              title="Customize IT"
+              handleClick ={()=>state.intro=false}
+              customStyles= " w-fit  px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
