2c0b18580587bb55a307ae2750bc06c6e2cfd5ca
Mahmudul Hasan <hasanmahmudul020@gmail.com>
2023-04-25 17:03:29 +0600

    logo added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import {
+  headContentAnimation,
+  headContainerAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Home</div>
-  )
-}
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.div className="home" {...slideAnimation("left")}>
+          <motion.header>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            ></img>
+          </motion.header>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default Home
+export default Home;
