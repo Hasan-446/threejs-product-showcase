440a3d0e339401de8ae45abedbfee889fe2dcd1e
Mahmudul Hasan <hasanmahmudul020@gmail.com>
2023-04-25 23:00:10 +0600

    update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,6 @@
 import CustomButton from "./CustomButton";
-export {CustomButton};
+import AiPicker from "./AiPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+export { CustomButton, ColorPicker, FilePicker, Tab, AiPicker };
