13978e8e54c5b46d08dc4b751d4556dbee3c9713
Mahmudul Hasan <hasanmahmudul020@gmail.com>
2023-04-25 21:47:46 +0600

    update

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <div>Customizer</div>;
+};
 
-export default Customizer
+export default Customizer;
