67e5faac6d6dfe83d70206241123aa3c287e5cd2
Mahmudul Hasan <hasanmahmudul020@gmail.com>
2023-05-16 01:38:28 +0600

    update

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import CustomButton from "./CustomButton";
 
-const AiPicker = () => {
+const AiPicker = ({ prompt, setPrompt, generateImg, handleSubmit }) => {
   return (
-    <div>AiPicker</div>
-  )
-}
+    <div className="aipicker-container">
+      <textarea
+        className="aipicker-textarea"
+        placeholder="write Your Prompt"
+        value={prompt}
+        rows={5}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className="flex flex-wrap gap-3">
+        {generateImg ? (
+          <CustomButton
+            type="outline"
+            title="asking AI ...."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default AiPicker
+export default AiPicker;
--- server/routes/dalle.router.js
+++ server/routes/dalle.router.js
@@ -8,24 +8,30 @@ const router = express.Router();
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-})
+});
 
-const openAi= new OpenAIApi(config);
+const openAi = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello World! dall e routes" });
 });
 
-router.route("/").post(async(req, res) => {
+router.route("/").post(async (req, res) => {
   try {
-    const {prompt}= req.body;
+    const { prompt } = req.body;
     const response = await openAi.createImage({
-      
-    })
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({message: "Something went wrong"})
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
-})
+});
 
 export default router;
