27045c7001ef52d0b95d60742fafec2ca83b6a63
Mahmudul Hasan <74701997+Hasan-446@users.noreply.github.com>
2023-06-07 12:33:49 +0600

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Order from "./pages/Order.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "./pages/SignIn.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
   {
     path: "order",
     element: <Order />,
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "signin",
+    element: <SignIn />,
   },
 ]);
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,25 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#eef2ff",
+          100: "#e0e7ff",
+          200: "#c7d2fe",
+          300: "#a5b4fc",
+          400: "#818cf8",
+          500: "#6366f1",
+          600: "#4f46e5",
+          700: "#4338ca",
+          800: "#3730a3",
+          900: "#312e81",
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
